bc5313a57f681e0ebf4838e11724e8e79f4320fa
Juti <jbuican19@gmail.com>
2025-04-18 15:52:44 +0100

    Update coffee_machine.py

--- day16/coffee_machine.py
+++ day16/coffee_machine.py
@@ -1,29 +1,42 @@
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Adjust the issue with the case where user press enter when insert coins 
+# this will then become 0 instead of crashing
+class MoneyMachine:
 
-from menu import Menu
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+    CURRENCY = "$"
 
-menu = Menu()
-coffee_maker = CoffeeMaker()
-money_machine = MoneyMachine()
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
 
-is_continue = True
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
 
-while is_continue:
-    choice = input(f"What would you like? {menu.get_items()}").lower().strip() or 'no idea'
-    if choice == 'report':
-        coffee_maker.report()
-    elif choice == 'off':
-        is_continue = False
-    elif choice == 'refill':
-        coffee_maker.refilled_coffee()
-    elif choice != (menu.find_drink(choice) == 'None'):
-        coffee = menu.find_drink(choice)
-        can_fulfill = coffee_maker.is_resource_sufficient(coffee)
-        if can_fulfill:
-            payment = money_machine.make_payment(coffee.cost)
-            if payment:
-                coffee_maker.make_coffee(coffee)
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
 
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ") or 0) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
 
