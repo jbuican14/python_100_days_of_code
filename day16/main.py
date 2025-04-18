cdc35cd673d50c0a0f3105212e3ed29a647369a6
Juti <jbuican19@gmail.com>
2025-04-18 17:17:19 +0100

    refactor main.py

--- day16/main.py
+++ day16/main.py
@@ -1,5 +1,6 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -14,14 +15,12 @@ while is_continue:
     choice = input(f"What would you like? {menu.get_items()}").lower().strip() or 'no idea'
     if choice == 'report':
         coffee_maker.report()
+        money_machine.report()
     elif choice == 'off':
         is_continue = False
     elif choice == 'refill':
         coffee_maker.refilled_coffee()
     elif choice != (menu.find_drink(choice) == 'None'):
         coffee = menu.find_drink(choice)
-        can_fulfill = coffee_maker.is_resource_sufficient(coffee)
-        if can_fulfill:
-            payment = money_machine.make_payment(coffee.cost)
-            if payment:
-                coffee_maker.make_coffee(coffee)
+        if coffee_maker.is_resource_sufficient(coffee) and money_machine.make_payment(coffee.cost):
+            coffee_maker.make_coffee(coffee)
